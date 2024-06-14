b08a4421edf25fdc7a8290cf096797eb226f3e24
Q-u1n <maqichao0203@gmail.com>
2024-06-13 17:47:44 -0700

    Create clietn.go

---
+++ clietn.go
@@ -0,0 +1,80 @@
+package kvraft
+
+import "6.5840/labrpc"
+import "crypto/rand"
+import "math/big"
+
+
+type Clerk struct {
+	server *labrpc.ClientEnd
+	// You will have to modify this struct.
+}
+
+func nrand() int64 {
+	max := big.NewInt(int64(1) << 62)
+	bigx, _ := rand.Int(rand.Reader, max)
+	x := bigx.Int64()
+	return x
+}
+
+func MakeClerk(server *labrpc.ClientEnd) *Clerk {
+	ck := new(Clerk)
+	ck.server = server
+	// You'll have to add code here.
+	return ck
+}
+
+// fetch the current value for a key.
+// returns "" if the key does not exist.
+// keeps trying forever in the face of all other errors.
+//
+// you can send an RPC with code like this:
+// ok := ck.servers[i].Call("KVServer."+op, &args, &reply)
+//
+// the types of args and reply (including whether they are pointers)
+// must match the declared types of the RPC handler function's
+// arguments. and reply must be passed as a pointer.
+func (ck *Clerk) Get(key string) string {
+
+	// You will have to modify this function.
+	args := &GetArgs{
+		Key: key,
+	}
+	reply := &GetReply{}
+	for !ck.server.Call("KVServer.Get", args, reply) {}
+	return reply.Value
+}
+
+// shared by Put and Append.
+//
+// you can send an RPC with code like this:
+// ok := ck.servers[i].Call("KVServer.PutAppend", &args, &reply)
+//
+// the types of args and reply (including whether they are pointers)
+// must match the declared types of the RPC handler function's
+// arguments. and reply must be passed as a pointer.
+func (ck *Clerk) PutAppend(key string, value string, op string) {
+	// You will have to modify this function.
+	MessageID := nrand()
+	arg := &PutAppendArgs {
+		Key :key,
+		Value: value,
+		MessageID: MessageID,
+		MessageType: Modify,
+	}
+	reply := &PutAppendReply{}
+	for !ck.server.Call("KVServer." + op, arg, reply) {}
+	arg = &PutAppendArgs {
+		MessageType: Report,
+		MessageID: MessageID,
+	}
+	for !ck.server.Call("KVServer." + op, arg, reply) {}
+	return reply.Value
+}
+
+func (ck *Clerk) Put(key string, value string) {
+	ck.PutAppend(key, value, "Put")
+}
+func (ck *Clerk) Append(key string, value string) {
+	ck.PutAppend(key, value, "Append")
+}
